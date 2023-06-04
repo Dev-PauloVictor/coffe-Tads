e656b338cabcf0b604205e62b6de5d949587937b
Paulo Victor <97921868+Dev-PauloVictor@users.noreply.github.com>
2023-06-04 20:53:36 -0300

    atualização V1.1 (add: guandar nomes)

--- main.js
+++ main.js
@@ -9,22 +9,34 @@ function retornarNomes(listaNomes) {
 function inserirNomes() {
   var NomesAlunos = [
     "André Oleari", "Antonio Paulo", "Antonio Rafael", "Emily Mayara", 
-    "Estelit Patricia", "Francisco Caio", "Jasmin Miranda", 
-    "Jheniffer da Silva", "João Miguel", "João Pedro", "João Victor", 
-    "Josias Rodrigues", "Keterlli Oliveira", "Lian Silva", "Luiz Silva", 
-    "Maria Rita", "Mateus Rodrigues", "Paulo Victor", "Weldison Gonzaga", 
-    "Vitor Soares", "Samya Tonielly", "Vagner Ribeiro"
+    "Estelit Patricia", "Francisco Caio",  
+    "Jheniffer da Silva", "João Pedro",
+    "Josias Rodrigues", "Keterlli Oliveira", "Luiz Silva", 
+    "Mateus Rodrigues","Weldison Gonzaga", 
+    "Vagner Ribeiro","Jackson Soeiros"
   ];
 
   var nomesSemana = [];
 
-  for (var i = 0; i < 5; i++) {
-    if (NomesAlunos.length > 0) {
-      var nomeSelecionado = retornarNomes(NomesAlunos);
-      nomesSemana.push(nomeSelecionado);
-    } else {
-      nomesSemana.push("Sem nomes disponíveis");
+  var ultimaExibicao = localStorage.getItem("ultimaExibicao");
+  var hoje = new Date().getTime();
+  
+  if (ultimaExibicao && (hoje - ultimaExibicao) < 2.5 * 24 * 60 * 60 * 1000) {
+    nomesSemana = JSON.parse(localStorage.getItem("nomesSemana")) || [];
+  
+  }else {
+
+    for (var i = 0; i < 5; i++) {
+      if (NomesAlunos.length > 0) {
+        var nomeSelecionado = retornarNomes(NomesAlunos);
+        nomesSemana.push(nomeSelecionado);
+      } else {
+        nomesSemana.push("Sem nomes disponíveis");
+      }
     }
+
+    localStorage.setItem("nomesSemana", JSON.stringify(nomesSemana));
+    localStorage.setItem("ultimaExibicao", hoje);
   }
 
   document.getElementById("nome1").innerHTML = nomesSemana[0] || "Sem nomes disponíveis";
@@ -33,6 +45,3 @@ function inserirNomes() {
   document.getElementById("nome4").innerHTML = nomesSemana[3] || "Sem nomes disponíveis";
   document.getElementById("nome5").innerHTML = nomesSemana[4] || "Sem nomes disponíveis";
 }
-
-
-
